0b6245b85da6aca7251db862d8f9e337d962c2b0
Muhammad Rizal Al Baihaqi <rizalbaihaqiiiiii@gmail.com>
2025-10-17 00:30:30 +0700

    add folder

---
+++ lib/screens/laporan_keuangan/laporan_keuangan_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class LaporanKeuanganScreen extends StatelessWidget {
+  const LaporanKeuanganScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Laporan Keuangan Screen'));
+  }
+}
