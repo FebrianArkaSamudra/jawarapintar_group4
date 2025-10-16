033805746a9ad9c21e850b88d0462e4b985d561a
Snapperr <mikaila082@gmail.com>
2025-10-17 00:53:11 +0700

    tes

--- lib/screens/mutasi_keluarga/mutasi_keluarga_screen.dart
+++ lib/screens/mutasi_keluarga/mutasi_keluarga_screen.dart
@@ -4,6 +4,6 @@ class MutasiKeluargaScreen extends StatelessWidget {
   const MutasiKeluargaScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Mutasi Keluarga Screen'));
+    return Center(child: Text('Mutasi Keluarga Screen Blablabla'));
   }
 }
